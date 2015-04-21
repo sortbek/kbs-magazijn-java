7296f6a93822492f5d4c02715f08f0743a54a91f
jeffwienen <jefwienen@gmail.com>
2015-04-21 13:10:37 +0200

    Version 0.0.2

--- src/asrs/Product.java
+++ src/asrs/Product.java
@@ -12,5 +12,35 @@ package asrs;
 public class Product {
     private int size;
     private int id;
+    private String status;
+
+    public Product() {
+        this.status = "In het schap";
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
         
+    
 }
