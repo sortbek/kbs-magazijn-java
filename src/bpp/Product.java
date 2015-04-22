37036a5d4f867e803df609b69cd8b7c16c375d9b
MarjoleinHulst <marjolein.hulst@hotmail.com>
2015-04-22 09:45:55 +0200

    no message

--- src/bpp/Product.java
+++ src/bpp/Product.java
@@ -4,6 +4,38 @@ import java.util.ArrayList;
 
 public class Product {
     
-    private ArrayList<String> producten = new ArrayList<>();
+    private String name;
+    private int idProduct;
+    // private int size;
+    
+    public Product(String n, int id ){
+    this.name = n;
+    this.idProduct = id;
+    }
+    
+    public String Getname(){
+    return name;
+    }
+    
+    public void Setname(String n){
+        this.name = n;
+    }
+    
+    public int GetidProduct(){
+    return idProduct;
+    }
+    
+    public void SetidProduct(int id){
+        this.idProduct = id;
+    }
+    
+    //public int Getsize(){
+    //return size;
+    //}
+    
+    //public void Setsize(int s){
+    //this.size = s;
+    //}
        
+    
 }
