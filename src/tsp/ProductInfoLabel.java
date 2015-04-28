384d3980fdd3bcd46a8b776e10537862ce7e2b6c
elledubi <k.durmusoglu@hotmail.nl>
2015-04-28 13:43:41 +0200

    test1

---
+++ src/tsp/ProductInfoLabel.java
@@ -0,0 +1,18 @@
+package tsp;
+
+import javax.swing.JLabel;
+
+public class ProductInfoLabel extends JLabel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public void setTextProductInfo() {
+		this.setText("test");
+		
+		 
+	}
+
+}
