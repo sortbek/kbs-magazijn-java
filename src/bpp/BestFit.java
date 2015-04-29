15c0ada96a12829081c0e187409cfd9cb6529079
MarjoleinHulst <marjolein.hulst@hotmail.com>
2015-04-29 14:18:50 +0200

    no message

--- src/bpp/BestFit.java
+++ src/bpp/BestFit.java
@@ -55,29 +55,104 @@ public class BestFit {
         return sizet;
     }
     
+        public int GetBigProduct() {
+        // groot product
+        int j = 0;
+        int sizeb = 0;
+        	while (producten.size() > j) {
+                   
+                        p = producten.get(j);
+                         if (sizeb < p.Getsize()){
+                         sizeb = p.Getsize();
+                         }
+                        j++;
+		}
+        return sizeb;
+    }
+        
+        public int GetSmallProduct(int s) {
+        // groot product
+        int j = 0;
+        int sizeb = 0;
+        	while (producten.size() > j) {
+                   
+                        p = producten.get(j);
+                         if (s <= p.Getsize()){
+                             if (j<s){
+                         sizeb = p.Getsize();
+                         }
+                         }
+                        j++;
+		}
+        return sizeb;
+    }
+        
+       
+        
+    
+   
     public void BF(){
     int i = GetSizeOrder();
+    int inhoudbox;
+    }
     
-    // kiezen dozen
-    while (i>20){
-        // maak een doos van 20 groot aan
-        i = i-20;
-        System.out.println("doos gemaakt van 20");
-        System.out.println(i);
-    }
-    while (i>10){
-      // maak een doos van 10 groot aan
-        i = i-10;
-        System.out.println("doos gemaakt van 10");
-        System.out.println(i);
-    }
-    while (i>1){
-      // maak een doos van 5 groot aan
-        i = i-5;
-        System.out.println("doos gemaakt van 5");
-        System.out.println(i);
-    }
-    }
+    // kiezen dozen en vullen dozen
+//    while (i>20){
+//        inhoudbox = 20;
+//        // maak een doos van 20 groot aan
+//        System.out.println("doos gemaakt van 20");
+//        System.out.println(i);
+//        GetBigProduct();
+//        System.out.println(GetBigProduct());
+//        i = i-GetBigProduct();
+//        //product met die maat uit de lijst halen en in de box zetten
+//        inhoudbox = inhoudbox - GetBigProduct();
+//        while (GetSmallProduct(inhoudbox) != 0){
+//        while (inhoudbox >= GetSmallProduct(inhoudbox)){
+//         System.out.println("product is "+GetSmallProduct(inhoudbox)+ " groot");
+//         //product met die maat uit de lijst halen en in de box zetten
+//         i = i-GetSmallProduct(inhoudbox);
+//         System.out.println("er is nog "+ i + "over ");
+//        }}
+//    }
+//    while (i>10){
+//        inhoudbox = 10;
+//      // maak een doos van 10 groot aan
+//        i = i-10;
+//        System.out.println("doos gemaakt van 10");
+//        System.out.println(i);
+//        GetBigProduct();
+//        System.out.println(GetBigProduct());
+//         i = i-GetBigProduct();
+//        //product met die maat uit de lijst halen en in de box zetten
+//        inhoudbox = inhoudbox - GetBigProduct();
+//        while (GetSmallProduct(inhoudbox) != 0){
+//        while (inhoudbox >= GetSmallProduct(inhoudbox)){
+//         System.out.println("product is "+GetSmallProduct(inhoudbox)+ " groot");
+//         //product met die maat uit de lijst halen en in de box zetten
+//         i = i-GetSmallProduct(inhoudbox);
+//         System.out.println("er is nog "+ i + "over ");
+//        }}
+//    }
+//    while (i>1){
+//        inhoudbox = 5;
+//      // maak een doos van 5 groot aan
+//        i = i-5;
+//        System.out.println("doos gemaakt van 5");
+//        System.out.println(i);
+//         i = i-GetBigProduct();
+//        //product met die maat uit de lijst halen en in de box zetten
+//        inhoudbox = inhoudbox - GetBigProduct();
+//        while (GetSmallProduct(inhoudbox) != 0){
+//        while (inhoudbox >= GetSmallProduct(inhoudbox)){
+//         System.out.println("product is "+GetSmallProduct(inhoudbox)+ " groot");
+//         //product met die maat uit de lijst halen en in de box zetten
+//         i = i-GetSmallProduct(inhoudbox);
+//         System.out.println("er is nog "+ i + "over ");
+//        }}
+//    }
+//    
+//    }
     
     
 }
---
+++ src/bpp/SQL_test.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bpp;
+
+/**
+ *
+ * @author Marjolein
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class SQL_test {
+
+    private String myDriver;
+    private String dbHost;
+    private String dbName;
+    private String uName;
+    private String uPass;
+    private Depository depository;
+    private BoxDepository boxDepository;
+
+    public SQL_test() {
+        this.myDriver = "com.mysql.jdbc.Driver";
+        this.dbHost = "jdbc:mysql://jeffreywienen.nl/";
+        this.dbName = "mydb";
+        this.uName = "kbs";
+        this.uPass = "Kbs123";
+    }
+    
+    
+}
+
+
